5fd8cfef6233ed683cfdd274cc3418a96e579094
EDUARDO FEITOSA BATISTA <25132675@CFP127ALUNO.LOCAL>
2025-08-19 13:35:15 -0300

    Nome das classes alteradas

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import funcionarios from "./funcionarios.json" with {type:'json'}
 
 function criarConteudo(funcionario){
     const opcao = document.getElementById('opcao')
-    const container = document.createElement('div')
+    const card = document.createElement('div')
     const img = document.createElement('img')
     const nome = document.createElement('h2')
     const funcao = document.createElement('p')
@@ -13,14 +13,14 @@ function criarConteudo(funcionario){
     nome.textContent = funcionario.nome
     funcao.textContent = funcionario.cargo
 
-    opcao.appendChild(container)
+    opcao.appendChild(card)
 
-    container.appendChild(img)
-    container.classList.add('container')
+    card.appendChild(img)
+    card.classList.add('card')
     img.classList.add('funcionario')
 
-    document.documentElement.style.setProperty('--nome', container.appendChild(nome))
-    document.documentElement.style.setProperty('--funcao', container.appendChild(funcao))
+    document.documentElement.style.setProperty('--nome', card.appendChild(nome))
+    document.documentElement.style.setProperty('--funcao', card.appendChild(funcao))
 }
 
 function carregarConteudos(){
